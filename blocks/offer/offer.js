c0e89d37f7d5a9f60e2106de224d7109e9720a98
praffuls <praffuls@adobe.com>
2025-01-21 07:22:01 +0530

    Code update

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -17,16 +17,7 @@ export default async function decorate(block) {
     return offer;
   });
   
-  block.innerHTML = 
-  <div class='banner-content' data-aue-type="reference" data-aue-filter="cf">
-    <div data-aue-prop="heroImage" data-aue-type="media" class='banner-detail' style="background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%);">
-      <p data-aue-prop="title" data-aue-type="text" class='headline'>${cfReq.title}</p>
-      <p data-aue-prop="description" data-aue-type="text" class='headline'>${cfReq.description}</p>
-      <img src="${cfReq.imagePath}"></img>
-      <p data-aue-prop="shortDescription" data-aue-type="text" class='short-description'>${cfReq.shortDescription}</p>
-      <p data-aue-prop="preTitle" data-aue-type="text" class='pretitle'>${cfReq.preTitle}</p>
-      <p data-aue-prop="detail" data-aue-type="richtext" class='detail'>${cfReq.description}</p>
-      <p class="button-container"><a data-aue-prop="ctaUrl" data-aue-type="text" href="${cfReq.ctaRedirectLink}" title="${cfReq.ctaButtonText}" class="button">${cfReq.ctaButtonText}</a></p>
-    </div>
-  </div>;
+  block.innerHTML = '<div class="banner-content" data-aue-type="reference" data-aue-filter="cf"><div data-aue-prop="heroImage" data-aue-type="media" class="banner-detail" style="background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%);"><p data-aue-prop="title" data-aue-type="text" class="headline">'+ cfReq.title + '</p>'+
+      '<p data-aue-prop="description" data-aue-type="text" class="headline">'+cfReq.description +'</p><img src="${cfReq.imagePath}"></img><p data-aue-prop="shortDescription" data-aue-type="text" class="short-description">'+cfReq.shortDescription+'</p>'+
+      '<p data-aue-prop="preTitle" data-aue-type="text" class="pretitle">'+cfReq.preTitle+'</p><p data-aue-prop="detail" data-aue-type="richtext" class="detail">'+cfReq.description+'</p><p class="button-container"><a data-aue-prop="ctaUrl" data-aue-type="text" href="'+cfReq.ctaRedirectLink+'" title="'+cfReq.ctaButtonText+'" class="button">'+cfReq.ctaButtonText+'</a></p></div></div>';
 }
